f69c8b026929d9fc9a1a9c04d28521b41c611ece
rankadian <dianthafafian@gmail.com>
2025-10-13 23:38:17 +0700

    update information about aplication & add identity from creator this app

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -8,7 +8,7 @@ class AboutScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tentang Aplikasi'),
-        backgroundColor: Colors.blue,
+        backgroundColor: const Color(0xFF667eea),
         foregroundColor: Colors.white,
       ),
       body: const Padding(
@@ -21,10 +21,14 @@ class AboutScreen extends StatelessWidget {
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
-            Text('Versi: 1.0.0', style: TextStyle(fontSize: 16)),
             SizedBox(height: 10),
             Text(
               'Aplikasi ini membantu Anda melacak dan mengelola pengeluaran harian Anda dengan mudah.',
+              style: TextStyle(fontSize: 16),
+            ),
+            Text('', style: TextStyle(fontSize: 16)),
+            Text(
+              'Dibuat Oleh Evan Diantha Fafian dari Jurusan Teknologi Informasi, Program Studi Sistem Informasi Bisnis, Politeknik Negeri Malang.',
               style: TextStyle(fontSize: 16),
             ),
           ],
